ca92ded3ae8d21ec850c6135196e43fe7f991b9d
Ankit Jain <ankii.jain@gmail.com>
2022-10-05 09:42:36 +0530

    Fix Nat Gateway route issue

--- modules/route_table/main.tf
+++ modules/route_table/main.tf
@@ -52,20 +52,6 @@ resource aws_route "nat_gateway_ipv4_routes" {
   }
 }
 
-## IPv6 Route for VPC NAT Gateway
-resource aws_route "nat_gateway_ipv6_routes" {
-
-  count = var.create_nat_gateway_routes ? 1 : 0
-
-  route_table_id              = aws_route_table.this.id
-  destination_ipv6_cidr_block = "::/0"
-  nat_gateway_id              = var.nat_gateway_id
-
-  timeouts {
-    create = "5m"
-  }
-}
-
 ## Route Table Association to subnet
 resource aws_route_table_association "this" {
   for_each = var.subnets
